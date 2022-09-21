78c7481b1e6e35af12e7b46a8f09e6003e06e4ea
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-09-21 13:55:55 -0400

    Updated

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,7 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
-        
+        int totalEggs=0;
         System.out.println(totalEggs);
     }   
 }
