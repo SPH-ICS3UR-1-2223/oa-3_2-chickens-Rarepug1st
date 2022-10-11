eec895b15366bb57f5e2c202c2ed6e5c2ccb61d4
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-10-11 13:32:04 -0400

    .

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -2,6 +2,19 @@ public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
         int totalEggs=0;
-        System.out.println(totalEggs);
+     int eggPerChicken=4;
+     int chickenCount=8;
+     int Totaldays=1;
+     totalEggs= eggPerChicken*chickenCount;
+ // tue
+     chickenCount++;
+     totalEggs+= eggPerChicken*chickenCount;
+     // wed 
+     chickenCount /=2;
+     totalEggs+= eggPerChicken*chickenCount;
+  //
+     
+    System.out.println(totalEggs);
+         
     }   
 }
--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,6 +1,17 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
+double dailyAverage =0;
+double monthlyAverage=0;
+double EggCount=0;
+double monthlyProfit=0;
+
+EggCount+=100;
+EggCount+=121;
+EggCount+=117;
+dailyAverage = EggCount/3.0;
+monthlyAverage = dailyAverage*30;
+
 
         System.out.println("Daily Average:   " +dailyAverage);
         System.out.println("Monthly Average: " +monthlyAverage);
